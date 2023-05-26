ada5bf29ab95ad4057068915e3c369355f0d9baa
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-05-26 21:46:11 +0200

    сhange the database connection address

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const secretKey = 'asdfgfjlAsdfweofuheo1rffwe!!asd,';
-const mongoPath = 'mongodb://localhost:27017/mestodb';
+const mongoPath = 'mongodb://127.0.0.1:27017/mestodb';
 const linkRegExp = /^(https?|ftp):\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_+.~#?&\/=]*$/;
 
 module.exports = {
